0f89f4645581fad031a3b1e76773324d1546166e
Hanna Lee <mani21na@gmail.com>
2019-08-15 03:31:36 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,23 @@
+=begin
+def reverse_each_word(string)
+  array = string.split(" ")
+  #new_spring = ""
+  array.each{|word| word.reverse!}
+  array.join(" ")
+end
+=end
+
+
+def reverse_each_word(string)
+  array = string.split(" ")
+  array.collect do |word|
+    word.reverse!
+  end
+  array.join(" ")
+end
+
+
+
+def reverse_each_word(array)
+  array.split(" ").collect {|x| x.reverse!}.join(" ")
+end
