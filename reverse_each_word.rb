91706177a47e13b60d50b91ea83f3e80761ec450
Hanna Lee <mani21na@gmail.com>
2019-08-15 03:29:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,13 @@
-
+=begin
 def reverse_each_word(string)
   array = string.split(" ")
   #new_spring = ""
   array.each{|word| word.reverse!}
   array.join(" ")
 end
+=end
 
 
-=begin
 def reverse_each_word(string)
   array = string.split(" ")
   array.collect do |word|
@@ -15,10 +15,9 @@ def reverse_each_word(string)
   end
   array.join(" ")
 end
-=end
 
-=begin
+
+
 def reverse_each_word(array)
   array.split(" ").collect {|x| x.reverse!}.join(" ")
 end
-=end
